e6a7ab43f6ab21525cfcae661fd217461fb5111c
virtuxa <dmitry-vidineeff@ya.ru>
2024-10-02 00:28:27 +0300

    Добавлен Index.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
